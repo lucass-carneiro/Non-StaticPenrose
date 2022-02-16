00d00772fa7144b4e338f6f44536941b7e64e797
Lucas Timotheo Sanches <lucas.t.s.carneiro@gmail.com>
2022-02-16 20:17:44 -0300

    Fixed undefined references in the Kerr-Schild Kerr metric plugin. Fixed wrong radius function that was producing erroneous metric results

--- plugins/metrics/KerrSchild_Kerr/include/aux_functions.hpp
+++ plugins/metrics/KerrSchild_Kerr/include/aux_functions.hpp
@@ -15,13 +15,6 @@ template <typename T> constexpr auto Power(T x, int n) -> double {
 
 constexpr auto Sqrt(auto x) { return std::sqrt(x); }
 
-auto r_KS_part_1(double a, double x, double y, double z) -> double;
-auto r_KS_part_2(double part1, double a, double z) -> double;
-
-auto r_KS_d_part_1_dxi(double xi) -> double;
-auto r_KS_d_part_2_dx_y(double part1, double part2, double dpart1dx_y) -> double;
-auto r_KS_d_part_2_dz(double part1, double part2, double dpart1dz, double a, double z) -> double;
-
 auto r_KS(double a, double x, double y, double z) -> double;
 
 auto d_r_KS_dx(double a, double x, double y, double z) -> double;
--- plugins/metrics/KerrSchild_Kerr/source/grad_lapse.cpp
+++ plugins/metrics/KerrSchild_Kerr/source/grad_lapse.cpp
@@ -19,90 +19,98 @@ GRLENSING_KERRSCHILD_KERR_METRIC_API auto KerrSchild_Kerr::grad_lapse(double, do
   auto dr_dy = [&](double x, double y, double z) { return d_r_KS_dy(a, x, y, z); };
   auto dr_dz = [&](double x, double y, double z) { return d_r_KS_dz(a, x, y, z); };
 
-  return metric_server::spatial_vector{
-      (M * Power(r(x, y, z), 2)
-       * (4 * M * x * Power(r(x, y, z), 4) * (Power(a, 2) + Power(r(x, y, z), 2))
-          - (3 * Power(a, 6) * Power(z, 2)
-             + r(x, y, z)
-                   * (4 * Power(a, 4) * M * Power(z, 2)
-                      + r(x, y, z)
-                            * (6 * Power(a, 4) * Power(z, 2)
-                               + r(x, y, z)
-                                     * (8 * Power(a, 2) * M * Power(z, 2)
-                                        + r(x, y, z)
-                                              * (-Power(a, 4) + 3 * Power(a, 2) * Power(z, 2)
-                                                 + 4 * M * (Power(x, 2) + Power(y, 2) + Power(z, 2))
-                                                       * r(x, y, z)
-                                                 - 2 * Power(a, 2) * Power(r(x, y, z), 2)
-                                                 - Power(r(x, y, z), 4))))))
-                * dr_dx(x, y, z)))
-          / (Power(Power(a, 4) * Power(z, 2) + 2 * Power(a, 2) * M * Power(z, 2) * r(x, y, z)
-                       + Power(a, 2) * Power(z, 2) * Power(r(x, y, z), 2)
-                       + 2 * M * (Power(x, 2) + Power(y, 2) + Power(z, 2)) * Power(r(x, y, z), 3)
-                       + Power(a, 2) * Power(r(x, y, z), 4) + Power(r(x, y, z), 6),
-                   2)
-             * Sqrt(
-                 1
-                 - (2 * M * Power(r(x, y, z), 3) * (Power(a, 2) + Power(r(x, y, z), 2)))
-                       / (Power(a, 4) * Power(z, 2) + 2 * Power(a, 2) * M * Power(z, 2) * r(x, y, z)
-                          + Power(a, 2) * Power(z, 2) * Power(r(x, y, z), 2)
-                          + 2 * M * (Power(x, 2) + Power(y, 2) + Power(z, 2)) * Power(r(x, y, z), 3)
-                          + Power(a, 2) * Power(r(x, y, z), 4) + Power(r(x, y, z), 6)))),
-      (M * Power(r(x, y, z), 2)
-       * (4 * M * y * Power(r(x, y, z), 4) * (Power(a, 2) + Power(r(x, y, z), 2))
-          - (3 * Power(a, 6) * Power(z, 2)
-             + r(x, y, z)
-                   * (4 * Power(a, 4) * M * Power(z, 2)
-                      + r(x, y, z)
-                            * (6 * Power(a, 4) * Power(z, 2)
-                               + r(x, y, z)
-                                     * (8 * Power(a, 2) * M * Power(z, 2)
-                                        + r(x, y, z)
-                                              * (-Power(a, 4) + 3 * Power(a, 2) * Power(z, 2)
-                                                 + 4 * M * (Power(x, 2) + Power(y, 2) + Power(z, 2))
-                                                       * r(x, y, z)
-                                                 - 2 * Power(a, 2) * Power(r(x, y, z), 2)
-                                                 - Power(r(x, y, z), 4))))))
-                * dr_dy(x, y, z)))
-          / (Power(Power(a, 4) * Power(z, 2) + 2 * Power(a, 2) * M * Power(z, 2) * r(x, y, z)
-                       + Power(a, 2) * Power(z, 2) * Power(r(x, y, z), 2)
-                       + 2 * M * (Power(x, 2) + Power(y, 2) + Power(z, 2)) * Power(r(x, y, z), 3)
-                       + Power(a, 2) * Power(r(x, y, z), 4) + Power(r(x, y, z), 6),
-                   2)
-             * Sqrt(
-                 1
-                 - (2 * M * Power(r(x, y, z), 3) * (Power(a, 2) + Power(r(x, y, z), 2)))
-                       / (Power(a, 4) * Power(z, 2) + 2 * Power(a, 2) * M * Power(z, 2) * r(x, y, z)
-                          + Power(a, 2) * Power(z, 2) * Power(r(x, y, z), 2)
-                          + 2 * M * (Power(x, 2) + Power(y, 2) + Power(z, 2)) * Power(r(x, y, z), 3)
-                          + Power(a, 2) * Power(r(x, y, z), 4) + Power(r(x, y, z), 6)))),
-      (M * Power(r(x, y, z), 2)
-       * (2 * z * r(x, y, z) * (Power(a, 2) + 2 * M * r(x, y, z))
-              * Power(Power(a, 2) + Power(r(x, y, z), 2), 2)
-          - (3 * Power(a, 6) * Power(z, 2)
-             + r(x, y, z)
-                   * (4 * Power(a, 4) * M * Power(z, 2)
-                      + r(x, y, z)
-                            * (6 * Power(a, 4) * Power(z, 2)
-                               + r(x, y, z)
-                                     * (8 * Power(a, 2) * M * Power(z, 2)
-                                        + r(x, y, z)
-                                              * (-Power(a, 4) + 3 * Power(a, 2) * Power(z, 2)
-                                                 + 4 * M * (Power(x, 2) + Power(y, 2) + Power(z, 2))
-                                                       * r(x, y, z)
-                                                 - 2 * Power(a, 2) * Power(r(x, y, z), 2)
-                                                 - Power(r(x, y, z), 4))))))
-                * dr_dz(x, y, z)))
-          / (Power(Power(a, 4) * Power(z, 2) + 2 * Power(a, 2) * M * Power(z, 2) * r(x, y, z)
-                       + Power(a, 2) * Power(z, 2) * Power(r(x, y, z), 2)
-                       + 2 * M * (Power(x, 2) + Power(y, 2) + Power(z, 2)) * Power(r(x, y, z), 3)
-                       + Power(a, 2) * Power(r(x, y, z), 4) + Power(r(x, y, z), 6),
-                   2)
-             * Sqrt(
-                 1
-                 - (2 * M * Power(r(x, y, z), 3) * (Power(a, 2) + Power(r(x, y, z), 2)))
-                       / (Power(a, 4) * Power(z, 2) + 2 * Power(a, 2) * M * Power(z, 2) * r(x, y, z)
-                          + Power(a, 2) * Power(z, 2) * Power(r(x, y, z), 2)
-                          + 2 * M * (Power(x, 2) + Power(y, 2) + Power(z, 2)) * Power(r(x, y, z), 3)
-                          + Power(a, 2) * Power(r(x, y, z), 4) + Power(r(x, y, z), 6))))};
+  metric_server::spatial_vector grad_lapse;
+  grad_lapse[0]
+      = (M * Power(r(x, y, z), 2)
+         * (4 * M * x * Power(r(x, y, z), 4) * (Power(a, 2) + Power(r(x, y, z), 2))
+            - (3 * Power(a, 6) * Power(z, 2)
+               + r(x, y, z)
+                     * (4 * Power(a, 4) * M * Power(z, 2)
+                        + r(x, y, z)
+                              * (6 * Power(a, 4) * Power(z, 2)
+                                 + r(x, y, z)
+                                       * (8 * Power(a, 2) * M * Power(z, 2)
+                                          + r(x, y, z)
+                                                * (-Power(a, 4) + 3 * Power(a, 2) * Power(z, 2)
+                                                   + 4 * M
+                                                         * (Power(x, 2) + Power(y, 2) + Power(z, 2))
+                                                         * r(x, y, z)
+                                                   - 2 * Power(a, 2) * Power(r(x, y, z), 2)
+                                                   - Power(r(x, y, z), 4))))))
+                  * dr_dx(x, y, z)))
+        / (Power(Power(a, 4) * Power(z, 2) + 2 * Power(a, 2) * M * Power(z, 2) * r(x, y, z)
+                     + Power(a, 2) * Power(z, 2) * Power(r(x, y, z), 2)
+                     + 2 * M * (Power(x, 2) + Power(y, 2) + Power(z, 2)) * Power(r(x, y, z), 3)
+                     + Power(a, 2) * Power(r(x, y, z), 4) + Power(r(x, y, z), 6),
+                 2)
+           * Sqrt(
+               1
+               - (2 * M * Power(r(x, y, z), 3) * (Power(a, 2) + Power(r(x, y, z), 2)))
+                     / (Power(a, 4) * Power(z, 2) + 2 * Power(a, 2) * M * Power(z, 2) * r(x, y, z)
+                        + Power(a, 2) * Power(z, 2) * Power(r(x, y, z), 2)
+                        + 2 * M * (Power(x, 2) + Power(y, 2) + Power(z, 2)) * Power(r(x, y, z), 3)
+                        + Power(a, 2) * Power(r(x, y, z), 4) + Power(r(x, y, z), 6))));
+  grad_lapse[1]
+      = (M * Power(r(x, y, z), 2)
+         * (4 * M * y * Power(r(x, y, z), 4) * (Power(a, 2) + Power(r(x, y, z), 2))
+            - (3 * Power(a, 6) * Power(z, 2)
+               + r(x, y, z)
+                     * (4 * Power(a, 4) * M * Power(z, 2)
+                        + r(x, y, z)
+                              * (6 * Power(a, 4) * Power(z, 2)
+                                 + r(x, y, z)
+                                       * (8 * Power(a, 2) * M * Power(z, 2)
+                                          + r(x, y, z)
+                                                * (-Power(a, 4) + 3 * Power(a, 2) * Power(z, 2)
+                                                   + 4 * M
+                                                         * (Power(x, 2) + Power(y, 2) + Power(z, 2))
+                                                         * r(x, y, z)
+                                                   - 2 * Power(a, 2) * Power(r(x, y, z), 2)
+                                                   - Power(r(x, y, z), 4))))))
+                  * dr_dy(x, y, z)))
+        / (Power(Power(a, 4) * Power(z, 2) + 2 * Power(a, 2) * M * Power(z, 2) * r(x, y, z)
+                     + Power(a, 2) * Power(z, 2) * Power(r(x, y, z), 2)
+                     + 2 * M * (Power(x, 2) + Power(y, 2) + Power(z, 2)) * Power(r(x, y, z), 3)
+                     + Power(a, 2) * Power(r(x, y, z), 4) + Power(r(x, y, z), 6),
+                 2)
+           * Sqrt(
+               1
+               - (2 * M * Power(r(x, y, z), 3) * (Power(a, 2) + Power(r(x, y, z), 2)))
+                     / (Power(a, 4) * Power(z, 2) + 2 * Power(a, 2) * M * Power(z, 2) * r(x, y, z)
+                        + Power(a, 2) * Power(z, 2) * Power(r(x, y, z), 2)
+                        + 2 * M * (Power(x, 2) + Power(y, 2) + Power(z, 2)) * Power(r(x, y, z), 3)
+                        + Power(a, 2) * Power(r(x, y, z), 4) + Power(r(x, y, z), 6))));
+  grad_lapse[2]
+      = (M * Power(r(x, y, z), 2)
+         * (2 * z * r(x, y, z) * (Power(a, 2) + 2 * M * r(x, y, z))
+                * Power(Power(a, 2) + Power(r(x, y, z), 2), 2)
+            - (3 * Power(a, 6) * Power(z, 2)
+               + r(x, y, z)
+                     * (4 * Power(a, 4) * M * Power(z, 2)
+                        + r(x, y, z)
+                              * (6 * Power(a, 4) * Power(z, 2)
+                                 + r(x, y, z)
+                                       * (8 * Power(a, 2) * M * Power(z, 2)
+                                          + r(x, y, z)
+                                                * (-Power(a, 4) + 3 * Power(a, 2) * Power(z, 2)
+                                                   + 4 * M
+                                                         * (Power(x, 2) + Power(y, 2) + Power(z, 2))
+                                                         * r(x, y, z)
+                                                   - 2 * Power(a, 2) * Power(r(x, y, z), 2)
+                                                   - Power(r(x, y, z), 4))))))
+                  * dr_dz(x, y, z)))
+        / (Power(Power(a, 4) * Power(z, 2) + 2 * Power(a, 2) * M * Power(z, 2) * r(x, y, z)
+                     + Power(a, 2) * Power(z, 2) * Power(r(x, y, z), 2)
+                     + 2 * M * (Power(x, 2) + Power(y, 2) + Power(z, 2)) * Power(r(x, y, z), 3)
+                     + Power(a, 2) * Power(r(x, y, z), 4) + Power(r(x, y, z), 6),
+                 2)
+           * Sqrt(
+               1
+               - (2 * M * Power(r(x, y, z), 3) * (Power(a, 2) + Power(r(x, y, z), 2)))
+                     / (Power(a, 4) * Power(z, 2) + 2 * Power(a, 2) * M * Power(z, 2) * r(x, y, z)
+                        + Power(a, 2) * Power(z, 2) * Power(r(x, y, z), 2)
+                        + 2 * M * (Power(x, 2) + Power(y, 2) + Power(z, 2)) * Power(r(x, y, z), 3)
+                        + Power(a, 2) * Power(r(x, y, z), 4) + Power(r(x, y, z), 6))));
+
+  return grad_lapse;
 }
