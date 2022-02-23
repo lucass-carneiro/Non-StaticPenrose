98029ed26f1d52d107d9d38d3ef364c988e2a32a
Lucas Timotheo Sanches <lucas.t.s.carneiro@gmail.com>
2022-02-23 15:41:14 -0300

    Made the tests aware of the rest of the code so that specific prts can be tested

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,5 +1,9 @@
+#include "cli.hpp"
+#include "log.hpp"
+
 #include <gtest/gtest.h>
 #include <mpi.h>
+#include <yaml-cpp/exceptions.h>
 
 constexpr auto Factorial(unsigned int number) -> unsigned int {
   return (number == 0 || number == 1) ? 1 : number * Factorial(number - 1);
@@ -16,10 +20,36 @@ TEST(FactorialTest, HandlesPositiveInput) {
 }
 
 auto main(int argc, char **argv) -> int {
-  MPI::Init(argc, argv);
+  using namespace grlensing;
 
-  ::testing::InitGoogleTest(&argc, argv);
-  auto test_run_code = RUN_ALL_TESTS();
+  int test_run_code = -1;
+
+  try {
+    MPI::Init(argc, argv);
+
+    // Load global configuration file
+    const auto config_file = YAML::LoadFile("../../configs/grlensing_testing_config.yaml");
+
+    // Initilize kernel
+    kernel proc_kernel;
+
+    // Load plugins
+    load_plugins(proc_kernel, config_file);
+
+    // Integrator base configuration
+    const integrator_config int_conf(config_file);
+
+    // Perform tests
+    ::testing::InitGoogleTest(&argc, argv);
+    test_run_code = RUN_ALL_TESTS();
+
+  } catch (YAML::Exception &e) {
+    if (MPI::COMM_WORLD.Get_rank() == 0) {
+      log<LogEvent::error>("Yaml parser error: {:s}", e.what());
+    }
+  } catch (std::exception &e) {
+    fmt::print("Error: {:s}\n", e.what());
+  }
 
   MPI::Finalize();
   return test_run_code;
